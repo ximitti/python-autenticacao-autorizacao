725fa3b17eee3947aa70ccde41204648adc4e253
Alex Schmitt <alex.schmitt@outlook.com>
2021-07-09 00:05:34 -0300

    refactor: disabled admin's functionalities

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -8,7 +8,3 @@ def init_app(app: Flask):
     from .users_view import bp as bp_users
 
     app.register_blueprint(bp_users)
-
-    from .admins_view import bp as bp_admins
-
-    app.register_blueprint(bp_admins)
